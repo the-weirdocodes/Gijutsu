66ad4b841f680dbd7de756879681a0cbccef95b1
Ushaswi Nandigam <ushaswinandigam@gmail.com>
2024-05-27 20:58:45 +0530

    Add files via upload

---
+++ konoha/Uchiha.java
@@ -0,0 +1,16 @@
+package konoha;
+public class Uchiha {
+public void Uchiha_Sasuke() {
+	System.out.println("Mangeyko Sharingan");
+}
+}
+// Let's start with the basic structure of java program, there are four parts, project->package->class->method. (Project name may not be visible)
+// Let's understand it hierarchically, Konoha (Hidden Leaf village) has different clans, each clan has members, each member has Kekkei Genkai. 
+// Similarly, Imagine Project as Fire nation, package as Konoha, Class as Uchiha Clan, Method as Uchiha Sasuke.
+// Let's put it simple, 
+//Project->Package->Class->Method
+//Fire Nation->Hidden Leaf Village->Uchiha->Uchiha Sasuke.
+//There's no fire nation without hidden villages like Konoha, there's no Konoha without it's clans, there's no clan with members in it.
+//Likewise, Project, Package, Class, Method are always "interdependent" on each other.
+
+//Let's see more about Classes and methods in the next session.

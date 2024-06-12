2beb8e23be003a2510f83e43a400e9fd88353e5f
Ushaswi Nandigam <ushaswinandigam@gmail.com>
2024-06-12 18:25:09 +0530

    Update Jinchuriki.java

--- konoha/Jinchuriki.java
+++ konoha/Jinchuriki.java
@@ -1,7 +1,7 @@
 package shinobi; 
 //let's print tailed beasts name ;)
 public class Jinchuriki {
-	String name; //instance property
+	String name; //instance member
 	// Instance property can be allocated in any kind of method unless it is static.
 	
 	public void Naruto() //non-static method
@@ -25,14 +25,14 @@ public class Jinchuriki {
 /*Let's dive into jutsu of Class,
 
 #Class: It's a combination of properties (data, variables) and behavior (method)
-#Weaving Strength and Chakra together makes a jutsu. Similarly, properties and behavior weave a class. 
+#Weaving Strength and Chakra together makes a jutsu. Similarly, properties, members and behavior weave a class. 
 #Now, these methods (behavior) are of two types, static and non-static.
 
 #Static method: 
 Remember ANBU Black Ops ? Those highly skilled masked Shinobi who directly work under the Hokage's direction personally?
 That's how exactly static method is prioritized in JVM (Java virtual Machine) for execution, this is because it allocates 
-memory directly from  the class while it is loading to compile. Static method needs static properties thus, doesn't accept 
-instances properties. 
+memory directly from  the class while it is loading to compile. Static method needs static members thus, doesn't accept 
+instances members. 
 
 #Non-Static method:
 This method allocates memory in the run time, basically executes slower than a static method. Allows instance properties 
@@ -40,7 +40,7 @@ into them. Can be used by any other behavior too.
 
  #Main-Method:
  This method is like Ichiraku Ramen for Naruto, he for sure visits Ichiraku Ramen everytime and enjoys Ramen there.
- Likewise each and every class, behavior and property comes to the main method for execution. It all starts with 
+ Likewise each and every class, behavior and members comes to the main method for execution. It all starts with 
  calling the class name first and creating an object. Object works like a surname. To call the non static methods 
  inside the class, we use the object, (object.method name). For static methods it's not necessary (Class name.method name).
  
